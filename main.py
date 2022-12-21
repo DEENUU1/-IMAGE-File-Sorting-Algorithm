f9765eebf332fd180950423f563e475426ba60f2
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-12-21 02:22:19 +0100

    everything works

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 from exif import Image
 import shutil
+import os
+import time
+
+start_time = time.time()
+
+dirs = {
+    '12': 'images/12/',
+    '11': 'images/11/',
+    '01': 'images/1/',
+    '02': 'images/2/',
+    '03': 'images/3/',
+    '04': 'images/4/',
+    '05': 'images/5/',
+    '06': 'images/6/',
+    '07': 'images/7/',
+    '08': 'images/8/',
+    '09': 'images/9/',
+    '10': 'images/10/',
+}
 
 source = 'images/'
-move_to = 'images/11/'
-month = []
 
-with open(source + 'IMG_7274.JPG', 'rb') as image_file:
-    my_image = Image(image_file)
+for filename in os.listdir(source):
+    f = os.path.join(source, filename)
 
-if my_image.has_exif == True:
-    date_time = my_image.datetime
-    for x in date_time:
-        month.append(x)
-    y = ''.join(month)
-    file_month = y[5:7]
+    if os.path.isfile(f):
+        file_names = f.replace('images/', '')
 
-    if file_month == '11':
-        shutil.copy(source+'IMG_7274.JPG', move_to)
-        print('Udało się przenieść plik')
+        try:
+            with open(f, 'rb') as image_file:
+                my_image = Image(image_file)
 
-else:
-    print("This picture doesn't have exif")
+                date_time = my_image.datetime
+                file_month = date_time[5:7]
+
+                move_to = dirs.get(file_month)
+        except:
+            print(f"This image {filename} doesn't have 'datetime' in exif.")
+            print("Move this file manually")
+        finally:
+            if move_to:
+                shutil.copy(f, move_to)
+                print(f"Files successfully transferred {filename} from {source} to {move_to}")
+                os.remove(f)
 
 
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+
+print(f'Code got executed in {elapsed_time} seconds.')
