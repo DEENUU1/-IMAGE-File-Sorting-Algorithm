4d16b16b12bc74fefae9364740ac409bce881914
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-12-21 23:34:45 +0100

    update

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from exif import Image
 import shutil
 import os
 import time
+from exif import Image
 
-# This is used for returning program speed
-start_time = time.time()
+
 
 # This dict represent paths for all months
 dirs = {
@@ -26,36 +25,42 @@ dirs = {
 source = 'images/'
 
 # Algorithm start with iterate through all files in source folder
-for filename in os.listdir(source):
-    f = os.path.join(source, filename)
+def sorting_algorithm(dirs:dict, source:str):
 
-    if os.path.isfile(f):
-        file_names = f.replace('images/', '')
+    # This is used for returning program speed
+    start_time = time.time()
 
-        try:
-            with open(f, 'rb') as image_file:
-                my_image = Image(image_file)
+    for filename in os.listdir(source):
+        f = os.path.join(source, filename)
 
-                #This code allows to get month from image
-                date_time = my_image.datetime
-                file_month = date_time[5:7]
+        if os.path.isfile(f):
+            file_names = f.replace('images/', '')
 
-                move_to = dirs.get(file_month)
-        # If image doesn't have datetime field program catch exception and continue
-        except:
-            print(f"This image {filename} doesn't have 'datetime' in exif.")
-            print("Move this file manually")
-        finally:
-            # If image has datetime field is transferred to the appropriate folder as a copy
-            # Then program removes image from main folder
-            # If the photo has not been moved, it will not be deleted
-            if move_to:
-                shutil.copy(f, move_to)
-                print(f"Files successfully transferred {filename} from {source} to {move_to}")
-                os.remove(f)
+            move_to = None
 
+            try:
+                with open(f, 'rb') as image_file:
+                    my_image = Image(image_file)
+                    #This code allows to get month from image
+                    date_time = my_image.datetime
+                    file_month = date_time[5:7]
+                    move_to = dirs.get(file_month)
+            # If image doesn't have datetime field program catch exception and continue
+            except:
+                print(f"This image {filename} doesn't have 'datetime' in exif.")
+                print("Move this file manually")
+            finally:
+                # If image has datetime field is transferred to the appropriate folder as a copy
+                # Then program removes image from main folder
+                # If the photo has not been moved, it will not be deleted
+                if move_to:
+                    shutil.copy(f, move_to)
+                    print(f"Files successfully transferred {filename} from {source} to {move_to}")
+                    os.remove(f)
 
-# After all program returns the time of the whole process
-end_time = time.time()
-elapsed_time = end_time - start_time
-print(f'Code got executed in {elapsed_time} seconds.')
+    # After all program returns the time of the whole process
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f'Code got executed in {elapsed_time} seconds.')
+
+sorting_algorithm(dirs,source)
